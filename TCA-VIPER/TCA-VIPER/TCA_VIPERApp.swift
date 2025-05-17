dc676dd8c542aee9cab8e9c7808afaae8d93693f
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-17 00:53:16 -0700

    initial commit

---
+++ TCA-VIPER/TCA-VIPER/TCA_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_VIPERApp.swift
+//  TCA-VIPER
+//
+//  Created by Ariel Tyson on 17/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
